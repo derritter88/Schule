558a4f412adc961ed698762f73413d3a8c45b92e
Mathias Muehlbacher <mathias.muehlbacher@gmx.at>
2014-12-26 15:27:17 +0100

    testet

---
+++ hallowelt/src/hallowelt/hallowelt.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package hallowelt;
+
+/**
+ * Mathias
+ * 26.12.2014
+ * hallowelt / hallowelt / hallowelt.java
+ */
+public class hallowelt
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hallo Welt!");
+	}
+
+}
